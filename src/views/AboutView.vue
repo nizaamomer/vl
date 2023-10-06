a8ed998d07da59f52e610936d062d5780e51d741
nizaamomer <nizaamomer@gmail.com>
2023-10-06 17:26:39 +0300

    #

---
+++ src/views/AboutView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1 ref="slaw">This is an about page</h1>
+  </div>
+</template>
+<script setup>
+import { ref } from "vue";
+const slaw = ref(null);
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1 ref="slaw">This is a home page</h1>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted, nextTick } from "vue";
+const slaw = ref(null);
+onMounted(async () => {
+  await nextTick(); // Wait for the next DOM update
+  console.log(slaw.value.offsetWidth);
+});
+</script>
